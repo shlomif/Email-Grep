f17b95822d3aab37951cfd9e516d695fa4f0958d
Shlomi Fish <shlomif@shlomifish.org>
2013-12-29 16:46:59 +0200

    Got dzil test --all to pass.

--- perl-Email-Grep/lib/Email/Grep.pm
+++ perl-Email-Grep/lib/Email/Grep.pm
@@ -3,5 +3,11 @@ package Email::Grep;
 use strict;
 use warnings;
 
+our $VERSION = '0.0.1';
+
 1;
 
+=head1 NAME
+
+Email::Grep - customisable command line filter (and pretty summariser) for
+E-mail messages.
