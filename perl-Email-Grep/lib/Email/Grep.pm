6417211f55fbe599ba4f16ce67cc82c5d6490281
Shlomi Fish <shlomif@shlomifish.org>
2019-04-04 23:53:35 +0300

    tidyall / dzil cleanups

--- perl-Email-Grep/lib/Email/Grep.pm
+++ perl-Email-Grep/lib/Email/Grep.pm
@@ -3,11 +3,11 @@ package Email::Grep;
 use strict;
 use warnings;
 
-our $VERSION = 'v0.0.1';
-
 1;
 
 =head1 NAME
 
 Email::Grep - customisable command line filter (and pretty summariser) for
 E-mail messages.
+
+=cut
